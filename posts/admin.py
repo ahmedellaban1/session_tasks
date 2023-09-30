284822726ce8e3b5a4154f6976ae36ab4d720ba4
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-09-30 09:24:54 +0300

    install taggit and pillow pkg

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish_date']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=5000)
+    publish_date = models.DateTimeField()
+    tags = TaggableManager()
