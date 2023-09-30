57d24168305f9fecc74dbae03549c3de8e738eb4
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-09-30 09:43:51 +0300

    add filter on post-admin

--- posts/admin.py
+++ posts/admin.py
@@ -6,6 +6,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'publish_date', 'auther']
     search_fields = ['title']
+    list_filter = ['title', 'publish_date', 'auther']
 
 
 admin.site.register(Post, PostAdmin)
