f6ed857c089019e9c95e56b39ff8f6ad3dd931ee
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-01 22:18:17 +0300

    add views for all posts, post details

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def posts_list(request, *args, **kwargs):
+    queryset = Post.objects.all()
+    context = {
+        "page_title": "all posts",
+        "posts": queryset,
+    }
+    return render(request, 'all-posts.html', context)
+
+
+def post_details(request, *args, **kwargs):
+    post = Post.objects.get(id=kwargs['id'])
+    context = {
+        "page_title": f"post {post.id} details",
+        "post": post,
+    }
+    return render(request, 'post-details.html', context)
