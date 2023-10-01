4c17ab6c4f2029eb6c5275143adc8b42e9a9309f
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-01 16:22:04 +0300

    add (fix) static url

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = ([
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+])
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
