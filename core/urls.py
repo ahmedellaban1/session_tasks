f46e916ec874d747401fc079ea1d3dea606b604c
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-01 23:09:02 +0300

    fix media & static url

--- core/urls.py
+++ core/urls.py
@@ -23,5 +23,5 @@ urlpatterns = ([
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls'))
 ])
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
