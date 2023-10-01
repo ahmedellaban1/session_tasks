a80f1ffd2804fd021f6554556afe3951c4445ad8
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-02 00:52:58 +0300

    urls for views all-posts, post_details

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = ([
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls'), name='posts')
 ])
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import posts_list, post_details
 
+app_name = 'posts'
+
 urlpatterns = [
     path('all-posts', posts_list, name='posts_list'),
     path('post-details/<int:id>', post_details, name='post_details'),
